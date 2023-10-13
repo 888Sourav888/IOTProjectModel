70442042aa517b68a178cde832f71d2937e98f58
888Sourav888 <77108272+888Sourav888@users.noreply.github.com>
2023-10-14 02:18:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,9 +84,9 @@ def indexFile():
 # app.run(port=port_no)
 
 
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
